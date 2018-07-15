80da551fd89e98a994d41c0b1f5d0f9513c721a3
Ahror Abdulhamidov <ab.ahror@gmail.com>
2018-07-15 01:05:55 -0700

    Update main.dart
    Comment blablabla

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import './ui/home.dart';
-// Run the Home Screen
+// Run the Home Screen 
 void main() {
   runApp(new MaterialApp(
     title: 'Salah Times',
     home: new Home(),
   ));
 }
+// Blablabla
